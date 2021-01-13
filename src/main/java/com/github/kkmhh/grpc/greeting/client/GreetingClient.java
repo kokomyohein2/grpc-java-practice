d5e071d5a2a249846f99e421a5ec7a3062299cee
Kokomyoheinhtet Unknown <kokomyoheinhtet@ascendcorp.com>
2021-01-13 15:04:59 +0700

    017 : Client SetupBoilerplate Code

--- src/main/java/com/github/kkmhh/grpc/greeting/client/GreetingClient.java
+++ src/main/java/com/github/kkmhh/grpc/greeting/client/GreetingClient.java
@@ -1,4 +1,22 @@
 package com.github.kkmhh.grpc.greeting.client;
 
+import com.proto.dummy.DummyServiceGrpc;
+import io.grpc.ManagedChannel;
+import io.grpc.ManagedChannelBuilder;
+
 public class GreetingClient {
+
+    public static void main(String[] args) {
+        System.out.println("Hello i am gRPC client");
+
+        ManagedChannel channel = ManagedChannelBuilder.forAddress("localhost",50051)
+                .usePlaintext()
+                .build();
+
+        System.out.println("Creating stub");
+        DummyServiceGrpc.DummyServiceBlockingStub syncClient = DummyServiceGrpc.newBlockingStub(channel);
+
+        System.out.println("Shutting down channel");
+        channel.shutdown();
+    }
 }
