721cb1e1b99a33dabd06d72f428c5fa6d8c601f1
Kokomyoheinhtet Unknown <kokomyoheinhtet@ascendcorp.com>
2021-01-13 16:13:38 +0700

    021 : Unary API Client Implementation

--- src/main/java/com/github/kkmhh/grpc/greeting/client/GreetingClient.java
+++ src/main/java/com/github/kkmhh/grpc/greeting/client/GreetingClient.java
@@ -1,6 +1,9 @@
 package com.github.kkmhh.grpc.greeting.client;
 
-import com.proto.dummy.DummyServiceGrpc;
+import com.proto.greet.GreetRequest;
+import com.proto.greet.GreetResponse;
+import com.proto.greet.GreetServiceGrpc;
+import com.proto.greet.Greeting;
 import io.grpc.ManagedChannel;
 import io.grpc.ManagedChannelBuilder;
 
@@ -9,12 +12,27 @@ public class GreetingClient {
     public static void main(String[] args) {
         System.out.println("Hello i am gRPC client");
 
-        ManagedChannel channel = ManagedChannelBuilder.forAddress("localhost",50051)
+        ManagedChannel channel = ManagedChannelBuilder.forAddress("localhost", 50051)
                 .usePlaintext()
                 .build();
 
         System.out.println("Creating stub");
-        DummyServiceGrpc.DummyServiceBlockingStub syncClient = DummyServiceGrpc.newBlockingStub(channel);
+//        DummyServiceGrpc.DummyServiceBlockingStub syncClient = DummyServiceGrpc.newBlockingStub(channel);
+
+        GreetServiceGrpc.GreetServiceBlockingStub greetClient = GreetServiceGrpc.newBlockingStub(channel);
+
+        Greeting greeting = Greeting.newBuilder()
+                .setFirstName("KoKo")
+                .setLastName("Unknown")
+                .build();
+
+        GreetRequest greetRequest = GreetRequest.newBuilder()
+                .setGreeting(greeting)
+                .build();
+
+        GreetResponse greetResponse = greetClient.greet(greetRequest);
+
+        System.out.println("Response : " + greetResponse.getResult());
 
         System.out.println("Shutting down channel");
         channel.shutdown();
